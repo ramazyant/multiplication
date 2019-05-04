4c5cb0e74dfbbdac8397d2b7ec930dfd12724021
ramazyant <43128834+ramazyant@users.noreply.github.com>
2019-05-04 21:05:21 +0300

    Update Multiplicator.h

--- Multiplicator.h
+++ Multiplicator.h
@@ -33,6 +33,7 @@ public:
         return res;
     }
     
+    /*
     // Divide and Conquer - DaC
 #include <iostream>
     Number DaC (Number & oper1, Number & oper2) {
@@ -64,8 +65,36 @@ public:
         x2.Shift(n / 2);
         return x1 + x2 + x3;
     }
+     */
+    
+    // Divide and Conquer - DaC
+    Number DaC (Number & oper1, Number & oper2) {
+        long size1 = oper1.size(), size2 = oper2.size();
+        long n = std::max(size1, size2);
+        if (n == 1) {
+            return oper1 * oper2;
+        } else {
+            Number left1, right1, left2, right2;
+            left1.Subnumber(oper1, 0, std::floor(size1/2));
+            right1.Subnumber(oper1, std::floor(size1/2) + 1, size1);
+            left2.Subnumber(oper2, 0, std::floor(size2/2));
+            right2.Subnumber(oper2, std::floor(size2/2) + 1, size2);
+            Number temp1 = left1 + right1, temp2 = left2 + right2;
+            //
+            Number x1 = DaC(right1, right2);
+            Number x3 = DaC(left1, left2);
+            Number x2 = DaC(temp1, temp2);
+            x2 = x2 - x1;
+            x2 = x2 - x3;
+            x1.Shift(n);
+            x2.Shift(n / 2);
+            x1 = x1 + x2;
+            x1 = x1 + x3;
+            return x1;
+        }
+    }
 };
-
+    
 Multiplicator::Number Multiplicator::res(1, 0);
 
 //output of a number
@@ -87,3 +116,36 @@ std::ostream & operator << (std::ostream & os, std::vector<std::pair<double, dou
 }
 
 #endif /* Multiplicator_h */
+
+
+
+
+/*
+    // Divide and Conquer - DaC
+    Number DaC (Number & oper1, Number & oper2) {
+        long size1 = oper1.size(), size2 = oper2.size();
+        long n = std::max(size1, size2);
+        if (n == 1) {
+            return oper1 * oper2;
+        } else {
+            Number left1(size1/2), right1(size1/2), left2(size2/2), right2(size2/2);
+            left1.Subnumber(oper1, 0, size1/2);
+            right1.Subnumber(oper1, size1/2, size1);
+            left2.Subnumber(oper2, 0, size2/2);
+            right2.Subnumber(oper2, size2, size2/2);
+            Number temp1 = left1 + right1, temp2 = left2 + right2;
+            //
+            Number x1 = DaC(right1, right2);
+            Number x3 = DaC(left1, left2);
+            Number x2 = DaC(temp1, temp2);
+            x2 = x2 - x1;
+            x2 = x2 - x3;
+            x1.Shift(n);
+            x2.Shift(n / 2);
+            x1 = x1 + x2;
+            x1 = x1 + x3;
+            
+        }
+    }
+};
+*/
